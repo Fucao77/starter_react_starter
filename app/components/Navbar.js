f8f51a7ebef6add3fb87fd59e5ae74dbff69791e
Fucao77 <axel.zidane@edu.devinci.fr>
2021-12-13 14:34:49 +0100

    add_navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+import styles from '../styles/Navbar.module.css'
+
+
+
+export default function Navbar() {
+   
+
+    return (
+        <div className={styles.navcontainer}>
+
+            <p>Toto</p>
+            <p>Slide</p>
+            <p>Session</p>
+            
+        </div>
+    );
+}
+
+
