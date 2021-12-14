6c4f74fc665cf06bde9e95598005f7c43068203f
Fucao77 <axel.zidane@edu.devinci.fr>
2021-12-14 09:30:37 +0100

    fix_card

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import styles from '../styles/Card.module.css'
 
 export default function Card(props) {
 
-
+    const [selected, setSelected] = useState(null);
+    useEffect(() => {
+        console.log(selected)
+    }, [selected]);
 
 
     return (
@@ -15,7 +18,7 @@ export default function Card(props) {
                 src="https://images.unsplash.com/photo-1418662589339-364ad47f98a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1164&q=80" />
             <p>{props.desc}</p>
             <h2>{props.price} euros</h2>
-            <div className={styles.button1}><button className={styles.button} >Acheter la carte</button></div>
+            <div className={styles.button1}><button className={styles.button} onClick={() => setSelected(props)} >Acheter la carte</button></div>
 
         </div>
 
